b1b438fba2fe5525ab8b8f1800700a062bf19024
mihir18bhawsar <mihir18bhawsar@gmail.com>
2022-04-04 09:10:50 +0530

    adjusted links to profile

--- client/src/Components/FriendCapsule/FriendCapsule.js
+++ client/src/Components/FriendCapsule/FriendCapsule.js
@@ -1,45 +1,56 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import PersonRemoveIcon from "@mui/icons-material/PersonRemove";
 import "./FriendCapsule.css";
 import { follow, unfollow } from "../../actions";
 class FriendCapsule extends React.Component {
-	handleFollow = () => {
+	handleFollow = (e) => {
+		e.preventDefault();
 		this.props.follow(this.props.user._id);
+		// e.stopPropagation();
 	};
-	handleUnfollow = () => {
+	handleUnfollow = (e) => {
+		e.preventDefault();
 		this.props.unfollow(this.props.user._id);
+		// e.stopPropagation();
 	};
 	render() {
 		if (this.props.user.disabled) return null;
 		return (
-			<div className="capsule-container">
-				<div className="profileContainer">
-					<img
-						src={
-							process.env.PUBLIC_URL +
-							`/images/${
-								this.props.user.profilePicture || "default.jpg"
-							}`
-						}
-						alt="profile"
-					/>
-				</div>
-				<div className="friendName">{this.props.user.username}</div>
+			<Link
+				classname="capsule-link"
+				to={`/profile/${this.props.user.username}`}
+			>
+				<div className="capsule-container">
+					<div className="profileContainer">
+						<img
+							src={
+								process.env.PUBLIC_URL +
+								`/images/${
+									this.props.user.profilePicture ||
+									"default.jpg"
+								}`
+							}
+							alt="profile"
+						/>
+					</div>
+					<div className="friendName">{this.props.user.username}</div>
 
-				{this.props.me.following.includes(this.props.user._id) ? (
-					<PersonRemoveIcon
-						className="mini-post-buttons"
-						onClick={this.handleUnfollow}
-					/>
-				) : (
-					<PersonAddIcon
-						className="mini-post-buttons"
-						onClick={this.handleFollow}
-					/>
-				)}
-			</div>
+					{this.props.me.following.includes(this.props.user._id) ? (
+						<PersonRemoveIcon
+							className="mini-post-buttons"
+							onClick={(e) => this.handleUnfollow(e)}
+						/>
+					) : (
+						<PersonAddIcon
+							className="mini-post-buttons"
+							onClick={(e) => this.handleFollow(e)}
+						/>
+					)}
+				</div>
+			</Link>
 		);
 	}
 }
